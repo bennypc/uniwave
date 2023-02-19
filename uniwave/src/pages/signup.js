c1d65f211fc24534be4cce93480685181a278bd9
bennypc <benny.pincha@gmail.com>
2023-02-18 16:26:25 -0800

    signup

--- uniwave/src/pages/signup.js
+++ uniwave/src/pages/signup.js
@@ -38,15 +38,15 @@ export default function Login() {
                 alt="Your Company"
               />
               <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
-                Sign in to your account
+                Create your uniwave account!
               </h2>
               <p className="mt-2 text-sm text-gray-600">
                 Or{' '}
                 <a
-                  href="/signup"
+                  href="/login"
                   className="font-medium text-indigo-600 hover:text-indigo-500"
                 >
-                  create a new account
+                  sign in to an existing account
                 </a>
               </p>
             </div>
